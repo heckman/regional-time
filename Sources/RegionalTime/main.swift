c2042724990df1cf7d58865040212fcf3ea6e1f5
Erik Ben Heckman <erik@heckman.ca>
2026-01-20 11:43:17 -0500

    Revert argument order to lat,long,seconds
    Because the location information is the important part.
    Seconds will be an optional argument in the future,
    if we're requesting location information that is not
    time-dependent.

--- Sources/RegionalTime/main.swift
+++ Sources/RegionalTime/main.swift
@@ -12,7 +12,7 @@ func die(_ message: String) {
 }
 
 class GeocoderDelegate: NSObject, CLLocationManagerDelegate {
-  func geocode(epochSeconds: Double, latitude: Double, longitude: Double) {
+  func geocode(latitude: Double, longitude: Double, epochSeconds: Double) {
     let location = CLLocation(latitude: latitude, longitude: longitude)
     let geoCoder = CLGeocoder()
     let dateToCheck = Date(timeIntervalSince1970: epochSeconds)
@@ -55,13 +55,13 @@ class GeocoderDelegate: NSObject, CLLocationManagerDelegate {
 }
 
 guard CommandLine.arguments.count == 4,
-  let epoch = Double(CommandLine.arguments[1]),
-  let lat = Double(CommandLine.arguments[2]),
-  let lon = Double(CommandLine.arguments[3])
+  let lat = Double(CommandLine.arguments[1]),
+  let lon = Double(CommandLine.arguments[2]),
+  let epoch = Double(CommandLine.arguments[3])
 else {
-  die("Usage: GeoTimeUtility <utc_epoch_seconds> <latitude> <longitude>")
+  die("Usage: GeoTimeUtility <latitude> <longitude> <utc_epoch_seconds>")
   exit(1)
 }
 
 let delegate = GeocoderDelegate()
-delegate.geocode(epochSeconds: epoch, latitude: lat, longitude: lon)
+delegate.geocode(latitude: lat, longitude: lon, epochSeconds: epoch)
